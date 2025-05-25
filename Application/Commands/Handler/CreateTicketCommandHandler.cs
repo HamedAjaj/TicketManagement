9f95dddb1a91618c88f0494922ef05c8ac17cafe
Hamed Farag Ajaj <57003501+HamedAjaj@users.noreply.github.com>
2025-05-25 12:16:20 +0300

    Update remove comments CreateTicketCommandHandler.cs

--- Application/Commands/Handler/CreateTicketCommandHandler.cs
+++ Application/Commands/Handler/CreateTicketCommandHandler.cs
@@ -24,7 +24,6 @@ namespace TicketManagement.Application.Commands.Handler
         {
             try
             {
-                // Validate the request data (optional - can be done in a higher layer).
                 if (string.IsNullOrWhiteSpace(request.PhoneNumber) ||
                     string.IsNullOrWhiteSpace(request.Governorate) ||
                     string.IsNullOrWhiteSpace(request.City) ||
@@ -60,42 +59,5 @@ namespace TicketManagement.Application.Commands.Handler
             }
         }
     }
-
-    //public class CreateTicketCommandHandler : IRequestHandler<CreateTicketCommand, ResponseWrapper<object>>
-
-    //{
-    //    private readonly TicketDbContext _context;
-    //    public CreateTicketCommandHandler(TicketDbContext context) => _context = context;
-
-    //    public async Task<ResponseWrapper<object>> Handle(CreateTicketCommand request, CancellationToken cancellationToken)
-    //    {
-    //        try
-    //        {
-    //            var ticket = new Ticket
-    //            {
-    //                PhoneNumber = request.PhoneNumber,
-    //                Governorate = request.Governorate,
-    //                City = request.City,
-    //                District = request.District
-    //            };
-    //            _context.Tickets.Add(ticket);
-    //            await _context.SaveChangesAsync(cancellationToken);
-    //            return new ResponseWrapper<object>
-    //            {
-    //                Success = true,
-    //                Data = ticket.Id
-
-    //            };
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            return new ResponseWrapper<object>
-    //            {
-    //                Success = false,
-    //                Errors = new List<string> { "An error occurred while creating the ticket.", ex.Message }
-    //            };
-    //        }
-    //    }
-    //}
 
 }
